bb73702aa1b73276ff73189a0ac74720fb738f21
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-06 01:17:55 -0300

    fix

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -4,7 +4,7 @@ class ManifestController < ApplicationController
     return render json: {} unless shop
 
     manifest = shop.manifest.as_json except: %i[id created_at updated_at shop_id]
-    manifest['icon'] = shop.manifest.icon.service_url.sub(/\?.*/, '') if shop.manifest.icon.present?
+    manifest['icon'] = shop.manifest.icon.service_url if shop.manifest.icon.present?
     render json: manifest
   end
 end
