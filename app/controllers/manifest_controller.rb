bcb79c4660fc9b413f335bd405658e82f0602fe6
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-08 19:09:33 -0300

    color picker

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -5,6 +5,8 @@ class ManifestController < ApplicationController
 
     manifest = shop.manifest.as_json except: %i[id created_at updated_at shop_id]
     manifest['icons'] = [shop.manifest.icon.service_url.sub(/\?.*/, '')] if shop.manifest.icon.present?
+    manifest['background_color'] = "#"+ manifest['background_color'].gsub("#","")
+    manifest['theme_color'] = "#"+ manifest['theme_color'].gsub("#")
     render json: manifest
   end
 end
--- app/javascript/shopify_app/flash_messages.js
+++ app/javascript/shopify_app/flash_messages.js
@@ -2,25 +2,27 @@ var eventName = typeof(Turbolinks) !== 'undefined' ? 'turbolinks:load' : 'DOMCon
 
 if (!document.documentElement.hasAttribute("data-turbolinks-preview")) {
   document.addEventListener(eventName, function flash() {
-    var flashData = JSON.parse(document.getElementById('shopify-app-flash').dataset.flash);
+    if (document.getElementById('shopify-app-flash')) {
+      var flashData = JSON.parse(document.getElementById('shopify-app-flash').dataset.flash);
 
-    var Toast = window['app-bridge'].actions.Toast;
+      var Toast = window['app-bridge'].actions.Toast;
 
-    if (flashData.notice) {
-      Toast.create(app, {
-        message: flashData.notice,
-        duration: 5000,
-      }).dispatch(Toast.Action.SHOW);
+      if (flashData.notice) {
+        Toast.create(app, {
+          message: flashData.notice,
+          duration: 5000,
+        }).dispatch(Toast.Action.SHOW);
+      }
+
+      if (flashData.error) {
+        Toast.create(app, {
+          message: flashData.error,
+          duration: 5000,
+          isError: true,
+        }).dispatch(Toast.Action.SHOW);
+      }
+
+      document.removeEventListener(eventName, flash)
     }
-
-    if (flashData.error) {
-      Toast.create(app, {
-        message: flashData.error,
-        duration: 5000,
-        isError: true,
-      }).dispatch(Toast.Action.SHOW);
-    }
-
-    document.removeEventListener(eventName, flash)
   });
 }
