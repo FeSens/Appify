76891d9987f91379a8550f9d3a58d3c663e03f42
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-09 00:02:49 -0300

    lets give it a try

--- app/javascript/packs/serviceworker-register.js
+++ app/javascript/packs/serviceworker-register.js
@@ -6,14 +6,7 @@ if (navigator.serviceWorker) {
 }
 window.addEventListener('beforeinstallprompt', e => {
   e.preventDefault();
-  e.prompt();
-  e.userChoice.userChoice.then((choice) => {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the saved prompt since it can't be used again
-    installPromptEvent = null;
-  });
+  window.installPromptEvent = e;
 });
+
+require("store_front")
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,15 +3,12 @@ ShopifyApp.configure do |config|
   config.api_key = Rails.application.credentials.dig(:shopify, :api_key)
   config.secret = Rails.application.credentials.dig(:shopify, :api_secret)
   config.old_secret = ""
-  config.scope = "read_products, write_script_tags, read_themes, write_themes" # Consult this page for more scope options:
+  config.scope = "write_script_tags, read_themes, write_themes" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2020-04"
   config.shop_session_repository = 'Shop'
-  config.scripttags = [
-    { event: 'onload', src: ->(domain) { "https://teste-giovanna.myshopify.com/apps/script/serviceworker-register.js" } }
-  ]
 end
 
 # ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
