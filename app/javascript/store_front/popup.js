44a08f9db13c636959af20004054543cf62e377b
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-10 21:26:52 -0300

    button bootstrap

--- app/javascript/store_front/popup.js
+++ app/javascript/store_front/popup.js
@@ -22,7 +22,22 @@ var style = {
     text-align: center;
     border-color: white;
     float: right;
-    padding: 10px 20px;`,
+    padding: 10px 20px;
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: .25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;`,
 
   alingBottom:`
     position: absolute;
