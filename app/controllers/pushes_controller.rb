e741c3a91457874ce2d5ff475316daf8f13a3f6b
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-13 02:18:12 -0300

    skip csfr

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -1,4 +1,6 @@
 class PushesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     push_subscriber = Push.find_or_initialize_by(endpoint: subscription_params[:endpoint])
     push_subscriber = push_subscriber.update(subscription_params)
