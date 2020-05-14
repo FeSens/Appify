1c375323df59f343a5e642917d7229c5cdfd333d
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-14 01:27:44 -0300

    maybe

--- app/controllers/send_pushes_controller.rb
+++ app/controllers/send_pushes_controller.rb
@@ -6,21 +6,7 @@ class SendPushesController < AuthenticatedController
         body: push_params[:body],
         icon: icon
       }
-      Webpush.payload_send(
-        endpoint: customer.endpoint,
-        message: message.to_json,
-        p256dh: customer.p256dh,
-        auth: customer.auth,
-        vapid: {
-          subject: 'mailto:suporte@appify.com',
-          public_key: Rails.application.credentials.dig(:webpush, :public_key),
-          private_key: Rails.application.credentials.dig(:webpush, :private_key)
-        }
-      )
-      true
-    rescue Webpush::ExpiredSubscription
-      customer.destroy
-      false
+      send(customer, message)
     end
   end
 
@@ -33,6 +19,24 @@ class SendPushesController < AuthenticatedController
   def icon
     return push_params[:icon] if push_params[:icon].present?
     shop.manifest.icon.variant(resize_to_fit: [192, 192]).processed.service_url.sub(/\?.*/, '') if shop.manifest.icon.present?
+  end
+
+  def send(customer, message)
+    Webpush.payload_send(
+      endpoint: customer.endpoint,
+      message: message.to_json,
+      p256dh: customer.p256dh,
+      auth: customer.auth,
+      vapid: {
+        subject: 'mailto:suporte@appify.com',
+        public_key: Rails.application.credentials.dig(:webpush, :public_key),
+        private_key: Rails.application.credentials.dig(:webpush, :private_key)
+      }
+      )
+    true
+  rescue Webpush::ExpiredSubscription
+    customer.destroy
+    false
   end
 
 end
