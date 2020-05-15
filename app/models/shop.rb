b7c357c0d267cdeff7d5ae864a36ee6b0639292f
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-15 19:19:04 -0300

    has many push interactions

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,13 +4,12 @@ class Shop < ActiveRecord::Base
   has_one :manifest, dependent: :destroy
   has_one :configuration, dependent: :destroy
   has_many :pushes, dependent: :destroy
-  has_one :push_interaction, dependent: :destroy
+  has_many :push_interactions, dependent: :destroy
   after_create :init_models
 
   def init_models
     create_manifest
     create_configuration
-    create_push_interaction
   end
 
   def api_version
