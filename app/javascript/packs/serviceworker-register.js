abed3be337fe85698ce3660e550f3c698dd7d798
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-19 11:21:28 -0300

    better modularization

--- app/javascript/packs/serviceworker-register.js
+++ app/javascript/packs/serviceworker-register.js
@@ -1,55 +1,13 @@
-import { idbKeyval } from 'indexdb'
-
-var vapidPublicKey = 'BOrPeoGdzvXg1OuNhjqYpCFof8D5QnDu4v1td5GTBBrXoVU-MhufANWOmWaHLH5ZXv3BUEFmP-I4m9Olme7V_VY';
-
-// move to utils
-function get_or_create_id() {
-  var id;
-  return idbKeyval.get("push-subscriber").then(function(result){
-    id = result
-  }).then(function() {
-    if (!id) { 
-      id = create_UUID()
-      idbKeyval.set("push-subscriber", id)
-    }
-    return id
-  })
-}
-
-function create_UUID() {
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (dt + Math.random()*16)%16 | 0;
-      dt = Math.floor(dt/16);
-      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-  });
-  return uuid;
-}
-
-//-------
+import { utils } from 'utils'
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/apps/script/serviceworker.js', { scope: '/' })
     .then(function(reg) {
       console.log('[Companion]', 'Service worker registered!');
-
-      return reg.pushManager.getSubscription()
-      .then(function(subscription) {
-        if (subscription) {
-          return subscription;
-        }
-        computeSubscriber("push")
-        return reg.pushManager.subscribe({                           //6
-          userVisibleOnly: true,
-          applicationServerKey: vapidPublicKey
-        });
-      });
-  }).then(function(subscription) {
-    sendKeys(subscription)                                           //7
-  });
+      utils.register_push_service(reg);
+  })
 }
 
-//PWA installer
 window.addEventListener('beforeinstallprompt', e => {
   e.preventDefault();
   window.installPromptEvent = e;
@@ -58,19 +16,3 @@ window.addEventListener('beforeinstallprompt', e => {
 window.addEventListener('appinstalled', () => {
   computeSubscriber("pwa")
 });
-//---
-
-//TODO move to utils
-function sendKeys(s){
-  return $.post('/apps/script/push', {
-    subscriber_id: get_or_create_id(),
-    endpoint: s.endpoint,
-    p256dh: btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('p256dh')))).replace(/\+/g, '-').replace(/\//g, '_'),
-    auth: btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('auth')))).replace(/\+/g, '-').replace(/\//g, '_')
-  });
-}
-
-function computeSubscriber(service) {
-  $.post('/apps/script/subscriber_count', { service });
-}
-//----
---
+++ app/javascript/utils/index.js
@@ -0,0 +1,57 @@
+import { idbKeyval } from 'indexdb'
+
+export let utils = (() => {
+  var vapidPublicKey = 'BOrPeoGdzvXg1OuNhjqYpCFof8D5QnDu4v1td5GTBBrXoVU-MhufANWOmWaHLH5ZXv3BUEFmP-I4m9Olme7V_VY';
+  
+  function get_or_create_id() {
+    var id;
+    return idbKeyval.get("push-subscriber").then(function(result){
+      id = result
+    }).then(function() {
+      if (!id) { 
+        id = create_UUID()
+        idbKeyval.set("push-subscriber", id)
+      }
+      return id
+    })
+  }
+
+  function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  }
+
+  function sendKeys(s) {
+    return $.post('/apps/script/push', {
+      subscriber_id: get_or_create_id(),
+      endpoint: s.endpoint,
+      p256dh: btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('p256dh')))).replace(/\+/g, '-').replace(/\//g, '_'),
+      auth: btoa(String.fromCharCode.apply(null, new Uint8Array(s.getKey('auth')))).replace(/\+/g, '-').replace(/\//g, '_')
+    });
+  }
+  
+  function computeSubscriber(service) {
+    $.post('/apps/script/subscriber_count', { service });
+  }
+
+  function register_push_service(reg) {
+    reg.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        return subscription;
+      }
+      computeSubscriber("push")
+      return reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: vapidPublicKey
+      }); 
+    }).then(function (subscription){
+      sendKeys(subscription) 
+    });
+  }
+})();
