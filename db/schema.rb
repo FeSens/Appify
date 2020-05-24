fd032e1a1b355fa64ef8cd2719647a32d75e1979
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-24 01:33:21 -0300

    add plan name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_031808) do
+ActiveRecord::Schema.define(version: 2020_05_24_043237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 2020_05_24_031808) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "domain"
     t.integer "push_limit", default: 200
+    t.string "plan_name"
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
   end
 
