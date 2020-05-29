6d430933c95f8531c6ccc2072753a04d25a865e1
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-29 11:36:05 -0300

    put manifest inside module

--- app/controllers/public/manifest_controller.rb
+++ app/controllers/public/manifest_controller.rb
@@ -1,23 +1,25 @@
-class ManifestController < PublicController
+module Public
+  class ManifestController < PublicController
 
-  def index
-    manifest = shop.manifest.as_json except: %i[id created_at updated_at shop_id]
-    manifest['icons'] = icons if shop.manifest.icon.present?
-    manifest['background_color'] = hash_color(manifest['background_color'])
-    manifest['theme_color'] = hash_color(manifest['theme_color'])
-    render json: manifest
-  end
-
-  def icons
-    sizes = [[192, 192], [512, 512]]
-    sizes.map do |size|
-      { src: shop.manifest.icon.variant(resize_to_fit: size).processed.service_url.sub(/\?.*/, ''),
-        sizes: "#{size[0]}x#{size[1]}",
-        type: shop.manifest.icon.content_type }
+    def index
+      manifest = shop.manifest.as_json except: %i[id created_at updated_at shop_id]
+      manifest['icons'] = icons if shop.manifest.icon.present?
+      manifest['background_color'] = hash_color(manifest['background_color'])
+      manifest['theme_color'] = hash_color(manifest['theme_color'])
+      render json: manifest
     end
-  end
 
-  def hash_color(color)
-    "##{color.gsub('#', '')}"
+    def icons
+      sizes = [[192, 192], [512, 512]]
+      sizes.map do |size|
+        { src: shop.manifest.icon.variant(resize_to_fit: size).processed.service_url.sub(/\?.*/, ''),
+          sizes: "#{size[0]}x#{size[1]}",
+          type: shop.manifest.icon.content_type }
+      end
+    end
+
+    def hash_color(color)
+      "##{color.gsub('#', '')}"
+    end
   end
 end
