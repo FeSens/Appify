d84c8e813bfd46584df84ba4fdfcff57b4a2c774
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-05-28 23:19:34 -0300

    Edit campaign button and new layout

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -1,5 +1,8 @@
 module Admin
   class CampaignsController < AuthenticatedController
+    before_action :load_campaign, only: %i[edit update]
+    after_action :create_job, only: %i[update create]
+    attr_accessor :campaign
     def index
       @campaigns = shop.campaigns.order(created_at: :desc)
     end
@@ -8,9 +11,15 @@ module Admin
       @campaign = shop.campaigns.new
     end
 
+    def edit; end
+
+    def update
+      campaign.update(campaing_params)
+      redirect_to admin_campaigns_path
+    end
+
     def create
-      campaign = shop.campaigns.create(campaing_params)
-      CreateCampaignJob.set(wait_until: campaign.release_date).perform_later(campaign)
+      @campaign = shop.campaigns.create(campaing_params)
       redirect_to admin_campaigns_path
     end
 
@@ -18,6 +27,14 @@ module Admin
 
     def campaing_params
       params.require(:campaign).permit(:name, :tag, :title, :body, :url, :release_date)
+    end
+
+    def load_campaign
+      @campaign = shop.campaigns.find(params[:id])
+    end
+
+    def create_job
+      CreateCampaignJob.set(wait_until: campaign.release_date).perform_later(campaign)
     end
   end
 end
