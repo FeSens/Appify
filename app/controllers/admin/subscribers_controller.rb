a0e144ba6040ce4ae3180a60516f62477d06b084
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-01 18:33:42 -0300

    stacked graph

--- app/controllers/admin/subscribers_controller.rb
+++ app/controllers/admin/subscribers_controller.rb
@@ -1,9 +1,19 @@
 module Admin
   class SubscribersController < AuthenticatedController
     def index
+      push_subs_base = SubscriberCount.where(['shop_id = ? and created_at < ?', shop.id, 14.days.ago]).push.count
+
+      push = []
+      push_new_subscribers = []
+      subscriber_count.push.pluck(:count).each do |p|
+        push <<  push_subs_base + push_new_subscribers.sum
+        push_new_subscribers << p
+      end
+
       subscriber_count = SubscriberCount.where(['shop_id = ? and created_at > ?', shop.id, 14.days.ago]).order(updated_at: :asc)
       render json: { pwa: subscriber_count.pwa.pluck(:count),
-                    push: subscriber_count.push.pluck(:count),
+                    push: push,
+                    push_new_subscribers: push_new_subscribers,
                     date_pwa: subscriber_count.pwa.pluck(:date),
                     date_push: subscriber_count.push.pluck(:date) }
     end
--- app/javascript/shopify_app/charts.js
+++ app/javascript/shopify_app/charts.js
@@ -9,17 +9,32 @@ $(document).on('turbolinks:load', function() {
         labels: data.date_push,
         datasets: [
           {
-            label: "Push",
-            backgroundColor: "#c45850",
+            maxBarThickness: 50,
+            label: "Subscribers",
+            backgroundColor: "#E95362",
             data: data.push
+          },
+          {
+            maxBarThickness: 50,
+            label: "New Subscribers",
+            backgroundColor: "#FF5B6B",
+            data: data.push_new_subscribers
           }
         ]
       },
       options: {
         legend: { display: false },
         title: {
-          display: true,
+          display: false,
           text: 'Assinantes Push'
+        },
+        scales: {
+          yAxes: [{
+            stacked: true,
+          }],
+          xAxes: [{
+            stacked: true,
+          }]
         }
       }
     });
