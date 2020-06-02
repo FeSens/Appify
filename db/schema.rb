e8e45dd687e34d431a0a7778a241a7b01345ee5b
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-02 00:56:32 -0300

    logo preview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_032124) do
+ActiveRecord::Schema.define(version: 2020_06_02_034059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,7 +151,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_032124) do
     t.string "domain"
     t.integer "push_limit", default: 200
     t.integer "plan_name", default: 0
-    t.string "shop_name"
+    t.string "name"
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
   end
 
