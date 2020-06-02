b1f1b084448cda748605acb5febdb5c58fff29e5
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-02 00:16:45 -0300

    shop models

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -14,21 +14,14 @@ class Shop < ActiveRecord::Base
   after_create :init_models
 
   def init_models
+    create_manifest
     self.optins.create(kind: 'pwa',
       title: "Put our store in your pocket!", body: "Download our app and keep updated about your order and the newest products!",
       accept_button: "Let's go!", timer: 15)
     self.optins.create(kind: 'push')
-
-    self.create_manifest(name: shop_name, short_name: shop_name)
   end
 
   def api_version
     ShopifyApp.configuration.api_version
-  end
-  
-  def shop_name
-    return 'Loja Teste' if Rails.env.development?
-
-    ShopifyAPI::Shop.current.name
   end
 end
