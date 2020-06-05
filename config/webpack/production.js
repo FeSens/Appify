27c78b27f2ceee6baec06239412da83941165d63
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-05 02:31:40 -0300

    go

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const merge = require('webpack-merge')
 const environment = require('./environment')
 
 const config = environment.toWebpackConfig()
