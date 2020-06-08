935ee761451bfcfd4d11a758b6215ed82ab79eb2
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-07 23:59:23 -0300

    graphql for the win

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -1,5 +1,5 @@
 class OrdersCreateJob < ActiveJob::Base
-  attr_accessor :webhook, :landing_site, :utm_source, :utm_medium, :utm_campaign, :campaign, :shop
+  attr_accessor :webhook, :utm_source, :utm_medium, :utm_campaign, :campaign, :shop
 
   def perform(shop_domain:, webhook:)
     @webhook = webhook
@@ -10,25 +10,32 @@ class OrdersCreateJob < ActiveJob::Base
     end
 
     shop.with_shopify_session do
-      @landing_site = ShopifyAPI::Order.find(webhook[:id], fields: 'landing_site')
+      utm_parameters
     end
-    return unless source_verified?
+    return unless utm_source == "aplicatify"
 
     @campaign = shop.campaigns.find_by(name: utm_campaign)
-
-    shop.orders.create(campaign_id: campaign&.id, landing_site: landing_site, total: webhook[:total_line_items_price], name: webhook[:name])
+    shop.orders.create(campaign_id: campaign&.id, total: webhook[:total_line_items_price], name: webhook[:name])
   end
 
-  def source_verified?
-    return false unless landing_site.include? "utm_source=aplicatify"
-    h = extract_parameters_from_url(landing_site)
-    @utm_medium, @utm_campaign = h[:utm_medium],  h[:utm_campaign]
-    true
-  end
-
-  def extract_parameters_from_url(url)
-    uri = URI.parse(url)
-    return Rack::Utils.parse_nested_query(uri.query)
-  end
-
+  def utm_parameters
+    query = ShopifyAPI::GraphQL.client.parse <<-'GRAPHQL'
+    {
+      order(id:"gid://shopify/Order/2298839728263") {
+        customerJourney {
+          lastVisit {
+            utmParameters {
+              campaign
+              medium
+              source
+            }
+          }
+        }
+      }
+    }
+    GRAPHQL
+    result = ShopifyAPI::GraphQL.client.query(query)
+    utm = result.data.order.customerJourney.lastVisit.utmParameters
+    @utm_medium, @utm_campaign, @utm_source = utm.medium, utm.campaign, utm.source
+  end  
 end
