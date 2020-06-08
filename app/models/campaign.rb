35ce93bd9e032db20031925285776608f14412ea
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-08 00:01:09 -0300

    needto check this

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ApplicationRecord
   belongs_to :shop
-  has_many :push_subscriber_campaigns, dependent: :destroy
-  has_many :pushes, through: :push_subscriber_campaigns, dependent: :destroy
+  has_many :push_subscriber_campaigns
+  has_many :pushes, through: :push_subscriber_campaigns
   has_many :orders, dependent: :nullify
   
   scope :sent, -> { where(arel_table[:release_date].lt(Time.now)) }
--- app/models/push.rb
+++ app/models/push.rb
@@ -1,8 +1,8 @@
 class Push < ApplicationRecord
   belongs_to :customer, optional: true
   belongs_to :shop, optional: true
-  has_many :push_subscriber_campaigns, dependent: :destroy
-  has_many :campaigns, through: :push_subscriber_campaigns, dependent: :destroy
+  has_many :push_subscriber_campaigns
+  has_many :campaigns, through: :push_subscriber_campaigns
   scope :last_month, -> { where('created_at > ?', 30.days.ago) }
   scope :last_half, -> { where('created_at > ?', 14.days.ago) }
   scope :active, -> { where('updated_at > ?', 12.hours.ago) }
