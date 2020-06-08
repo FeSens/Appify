6222b69b5599618b101d265af9cdccdd000f30b9
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-07 21:05:31 -0300

    remove coments

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,8 +3,6 @@
 module Admin
   class HomeController < AuthenticatedController
     def index
-      # @manifest = shop.manifest
-      # @configuration = shop.configuration
       push = PushInteraction.find_or_create_by(shop_id: shop.id, date: Date.today.at_beginning_of_month)
       @push_interaction = { push_count: push.count, push_limit: shop.push_limit }
       @new_subscribers = shop.subscriber_counts.push.last_half.count
