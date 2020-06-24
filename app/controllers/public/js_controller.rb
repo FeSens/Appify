e6b7e42b3d75ca459a878ae60c90e96e71dfa489
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-24 18:31:09 -0300

    lets add a 5 minutes caching scheme

--- app/controllers/public/js_controller.rb
+++ app/controllers/public/js_controller.rb
@@ -1,9 +1,11 @@
 module Public
   class JsController < PublicController
     def index
-      pwa  = Optin.find_by(shop_id: shop.id, kind: 'pwa')
-      push = Optin.find_by(shop_id: shop.id, kind: 'push')
-      @optins = { pwa: pwa, push: push }
+      Rails.cache.fetch("{JsController:#{shop.id}}", expires_in: 5.minutes) do
+        pwa  = Optin.find_by(shop_id: shop.id, kind: 'pwa')
+        push = Optin.find_by(shop_id: shop.id, kind: 'push')
+        @optins = { pwa: pwa, push: push }
+      end
     end
   end
 end
