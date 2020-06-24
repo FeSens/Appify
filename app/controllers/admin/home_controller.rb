c4be8c43ac504aaf06f99d46fd2f4fb129b46d49
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-24 17:27:42 -0300

    fix subscribers count

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -5,7 +5,7 @@ module Admin
     def index
       push = PushInteraction.find_or_create_by(shop_id: shop.id, date: Date.today.at_beginning_of_month)
       @push_interaction = { push_count: push.count, push_limit: shop.push_limit }
-      @new_subscribers = shop.subscriber_counts.push.last_half.count
+      @new_subscribers = shop.subscriber_counts.push.last_half.sum(:count)
       @push_subscribers = shop.pushes.count
       @campaigns = shop.campaigns
       @revenue = shop.orders.this_month.sum(:total)
