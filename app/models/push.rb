74ce064cc4c2583c4197383e77a654eb4b5ae0a6
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-25 16:16:17 -0300

    allow to destory push users

--- app/models/push.rb
+++ app/models/push.rb
@@ -3,7 +3,7 @@ class Push < ApplicationRecord
   belongs_to :shop, optional: true
   has_many :push_subscriber_campaigns
   has_many :campaigns, through: :push_subscriber_campaigns
-  has_many :page_visits
+  has_many :page_visits, dependent: :nullify
   scope :last_month, -> { where('created_at > ?', 30.days.ago) }
   scope :last_half, -> { where('created_at > ?', 14.days.ago) }
   scope :active, -> { where('updated_at > ?', 12.hours.ago) }
