7340332b8ff99682a60c905c4c4c26b23a35986f
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-06-29 13:27:50 -0300

    manifest controller rendered by erb

--- app/controllers/public/manifest_controller.rb
+++ app/controllers/public/manifest_controller.rb
@@ -1,24 +1,18 @@
 module Public
   class ManifestController < PublicController
-    def index
-      manifest = shop.manifest.as_json except: %i[id created_at updated_at shop_id]
-      manifest['icons'] = icons if shop.manifest.icon.present?
-      manifest['background_color'] = hash_color(manifest['background_color'])
-      manifest['theme_color'] = hash_color(manifest['theme_color'])
-      render json: manifest
-    end
+    helper_method :hash_color, :icon_sizes
 
-    def icons
-      sizes = [[192, 192], [512, 512]]
-      sizes.map do |size|
-        { src: shop.manifest.icon.variant(resize: size).processed.service_url.sub(/\?.*/, ''),
-          sizes: "#{size[0]}x#{size[1]}",
-          type: shop.manifest.icon.content_type }
-      end
+    def index
+      @manifest = shop.manifest
     end
 
     def hash_color(color)
       "##{color.gsub('#', '')}"
     end
+
+    def icon_sizes
+      ["192x192", "512x512"]
+    end
+
   end
 end
