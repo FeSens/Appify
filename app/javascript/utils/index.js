fb48ad0b94d6f7d4c5ebebdf5a3090e3bdb5e3e1
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-07-03 13:46:04 -0300

    test

--- app/javascript/utils/index.js
+++ app/javascript/utils/index.js
@@ -87,7 +87,7 @@ export let utils = (() => {
       .then((data) => {
         // Work with JSON data here
         $.post('/apps/script/analytics/carts', {
-          subscriber_id: dbKeyval.get("push-subscriber"),
+          subscriber_id: idbKeyval.get("push-subscriber"),
           token: data['token'],
           data: JSON.stringify(data)
         });
