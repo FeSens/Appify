c745bcd3ee1ee024134eafc05be1a48ef968606b
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-07-15 16:51:56 -0300

    Lets find the carts that are missing

--- app/jobs/checkouts_create_job.rb
+++ app/jobs/checkouts_create_job.rb
@@ -1,10 +1,11 @@
 class CheckoutsCreateJob < ActiveJob::Base
+  class CartNotFoundError < StandardError ; end
   queue_as :low
   attr_accessor :webhook
   def perform(shop_domain:, webhook:)
     @webhook = webhook
     cart = Cart.find_by(token: webhook[:cart_token])
     cart.update(abandoned: false) if cart
-    raise StandardError
+    raise CartNotFoundError unless cart
   end
 end
