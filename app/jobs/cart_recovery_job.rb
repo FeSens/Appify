a41689125a9c4551a728f0c6d15fd773ddcbf033
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-07-27 15:51:12 -0300

    this job is sick

--- app/jobs/cart_recovery_job.rb
+++ app/jobs/cart_recovery_job.rb
@@ -6,11 +6,15 @@ class CartRecoveryJob < ApplicationJob
       automation = shop.automations.cart_recovery
       return unless automation.enabled
 
-      pushes = shop.carts.where('updated_at < ? and tries < ?', automation.delay.hours.ago, automation.max_tries).abandoned.recoverable.pluck(:push_id)
+      pushes = shop.carts.where('updated_at < ? and tries < ? and push_id is not null', automation.delay.hours.ago, automation.max_tries).abandoned.recoverable.pluck(:push_id)
+      pushes = PushSubscriberCampaign.find_by_sql(
+        ['SELECT push_id from (SELECT push_id, max(created_at) as "latest" FROM push_subscriber_campaigns WHERE push_id in (?) GROUP BY push_id) as foo where foo.latest < ?', pushes, automation.delay.hours.ago]
+      ).pluck(:push_id)
       pushes = Push.find(pushes)
-      
+
+      n = shop.campaigns.where("name ILIKE ?", "%Cart Recovery%").count
       campaign = shop.campaigns.create(
-        name: "Cart Recovery", 
+        name: "Cart Recovery #{n+1}", 
         tag: "cart recovery automation", 
         title: automation.title, 
         body: automation.body, 
