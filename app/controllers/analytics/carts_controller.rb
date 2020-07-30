528034e48dca3142e93da039fe46a78b8fb5dd5d
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-07-30 15:53:56 -0300

    only set if true

--- app/controllers/analytics/carts_controller.rb
+++ app/controllers/analytics/carts_controller.rb
@@ -9,7 +9,9 @@ module Analytics
     def cart_params
       subscriber_id = params[:subscriber_id]
       p = Push.find_by(subscriber_id: subscriber_id)
-      params.permit(:token, :hexdigest, :utm_medium, :utm_campaign, :utm_source, data:{}).merge(push_id: p.present? ? p.id : nil, shop_id: shop.id, impacted: params['utm_source']&.include?('aplicatify'))
+      x = params.permit(:token, :hexdigest, :utm_medium, :utm_campaign, :utm_source, data:{}).merge(push_id: p.present? ? p.id : nil, shop_id: shop.id)
+      x = x.merge(impacted: true) if params['utm_source']&.include?('aplicatify')
+      return x
     end
   end
 end
