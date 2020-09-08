922ae7bf654fad4cb23ea034ff9e52fe7c1d444a
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-09-08 16:38:28 -0300

    clean the home controller

--- app/helpers/admin/shop_helper.rb
+++ app/helpers/admin/shop_helper.rb
@@ -20,5 +20,34 @@ module Admin
     def theme_verified?
       current_shop.theme_verified
     end
+
+    def campaigns
+      current_shop.campaigns
+    end
+
+    def revenue
+      @revenue ||= current_shop.orders.sum(:total)
+      number_to_currency(@revenue)
+    end
+
+    def app_revenue
+      @app_revenue ||= current_shop.campaigns.find_by(name:"app").orders.sum(:total)
+      number_to_currency(@app_revenue)
+    end
+
+    def new_subscribers
+      @new_subscribers ||= current_shop.subscriber_counts.push.last_half.sum(:count)
+    end
+
+    def push_interaction
+      @push_interaction ||= begin
+        push = PushInteraction.find_or_create_by(shop_id: current_shop.id, date: Date.today.at_beginning_of_month)
+        { push_count: push.count, push_limit: current_shop.push_limit }
+      end
+    end
+
+    def percentage_of_interactions_consumed
+      100 * push_interaction[:push_count].to_f / push_interaction[:push_limit].to_f
+    end
   end
 end
