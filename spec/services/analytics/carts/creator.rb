408b4df79e3880e7304643980901116fab300125
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-09-14 16:21:37 -0300

    init carts creator

---
+++ spec/services/analytics/carts/creator.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Analytics::Carts::Creator do
+  let(:shop) { FactoryBot.create :shop}
+  let(:campaign) { FactoryBot.create :campaign, shop: shop }
+  let(:push_interaction) { FactoryBot.create :push_interaction, shop: shop }
+  let(:other_push_interaction) { FactoryBot.create :push_interaction }
+
+  describe "increment clicks" do
+    it { expect {described_class.call(campaign.shop_id, campaign.id,  "clicks")}.to change { Campaign.find(campaign.id).clicks }.by(1) }
+  end
+
+  describe "increment impressions" do
+    it { expect {described_class.call(campaign.shop_id, campaign.id,  "impressions")}.to change { Campaign.find(campaign.id).impressions }.by(1) }
+    it { expect {described_class.call(campaign.shop_id, campaign.id,  "impressions")}.to change { PushInteraction.find(push_interaction.id).count }.by(1) }
+    it { expect {described_class.call(campaign.shop_id, campaign.id,  "impressions")}.not_to change { PushInteraction.find(other_push_interaction.id).count } }
+  end
+end
