0511f95eca316f692852ad1dcff0ae49269a4f4c
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-10-25 23:45:27 -0300

    add puma metrics

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+require 'barnes'
+
+before_fork do
+  # worker specific setup
+
+  Barnes.start # Must have enabled worker mode for this to block to be called
+end
