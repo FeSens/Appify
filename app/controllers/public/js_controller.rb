743eccb83765a08ca9496bab8490380c47c510d6
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-10-27 00:47:13 -0300

    lets cache some queries

--- app/controllers/public/js_controller.rb
+++ app/controllers/public/js_controller.rb
@@ -1,9 +1,11 @@
 module Public
   class JsController < PublicController
     def index
-      pwa  = Optin.find_by(shop_id: shop.id, kind: "pwa")
-      push = Optin.find_by(shop_id: shop.id, kind: "push")
-      @optins = { pwa: pwa, push: push }
+      Rails.cache.fetch("public/jscontroller/#{shop.id}", expires_in: 5.seconds) do
+        pwa  = Optin.find_by(shop_id: shop.id, kind: "pwa")
+        push = Optin.find_by(shop_id: shop.id, kind: "push")
+        @optins = { pwa: pwa, push: push }
+      end
     end
   end
 end
