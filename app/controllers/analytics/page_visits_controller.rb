e8d32e12998c780c89e41fbd84a9c48166c54368
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-11-03 16:24:05 -0300

    no more updates

--- app/controllers/analytics/page_visits_controller.rb
+++ app/controllers/analytics/page_visits_controller.rb
@@ -2,11 +2,11 @@ module Analytics
   class PageVisitsController < AnalyticsController
     def create
       params_recived = page_visit_params
-      p = Push.find_by(subscriber_id: params[:subscriber_id]) if params[:subscriber_id].present?
-      if p.present?
-        PageVisit.where(subscriber_id: p.subscriber_id, push_id: nil).update(push_id: p.id)
-        params_recived.merge(push_id: p.id)
-      end
+      #p = Push.find_by(subscriber_id: params[:subscriber_id]) if params[:subscriber_id].present?
+      #if p.present?
+      #  PageVisit.where(subscriber_id: p.subscriber_id, push_id: nil).update(push_id: p.id)
+      #  params_recived.merge(push_id: p.id)
+      #end
 
       PageVisit.create(params_recived)
       head :no_content
