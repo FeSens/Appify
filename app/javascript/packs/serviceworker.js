8d83261c53006a645c59a0cbf26e90e126cce88a
Felipe Bonetto <felipesensbonetto@gmail.com>
2020-11-11 18:12:02 -0300

    give me the host name

--- app/javascript/packs/serviceworker.js
+++ app/javascript/packs/serviceworker.js
@@ -22,7 +22,7 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('fetch', function (event) {
   const url = new URL(event.request.url)
-  console.log("flag A")
+  console.log(url.host)
   if(url.host === "cdn.shopify.com") {
     console.log('flag B')
     var req = new Request(event.request, {
