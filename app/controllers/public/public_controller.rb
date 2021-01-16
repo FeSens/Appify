580278ad6ec88a79dbab28ee1ede9e26957a2355
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-01-16 20:09:02 -0300

    update

--- app/controllers/public/public_controller.rb
+++ app/controllers/public/public_controller.rb
@@ -1,15 +1,15 @@
 module Public
-  class PublicController < ApplicationController
+  class PublicController < ActionController::Base
     skip_before_action :verify_authenticity_token
     skip_after_action :intercom_rails_auto_include
     before_action :validate
     attr_accessor :shop
 
     def validate
-      return @shop = Shop.last if Rails.env.development?
+      #return @shop = Shop.last if Rails.env.development?
 
       @shop = Rails.cache.fetch("Public/PublicController/#{params[:shop]}/#{params[:h]}", expires_in: 60.seconds) do
-        return Shop.find(params[:id]) if params[:h].present?
+        return Shop.find(params[:h]) if params[:h].present?
 
         Shop.find_by(shopify_domain: params[:shop])
       end
