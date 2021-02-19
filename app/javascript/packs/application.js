ef3958da706250f78db60e82aeadbf604e1ed050
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-02-19 16:38:05 -0300

    css and javascript trought webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("shopify_app")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,12 +16,14 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("shopify_app")
-
 import "bootstrap";
+import "../stylesheets/application"
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+  $('.toast').toast({ autohide: false })
+  $('#toast').toast('show')
 })
 
 window.$ = $;
