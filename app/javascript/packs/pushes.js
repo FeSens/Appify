38e0a0f1235aee8b3f9078787ec4ab90a51b9b4f
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-03-04 10:57:48 -0300

    better pushes?

--- app/javascript/packs/pushes.js
+++ app/javascript/packs/pushes.js
@@ -3,14 +3,40 @@ import { utils } from 'universal-utils'
 function install() {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('aplicatify-serviceworker.js', { scope: '/' })
-      .then(function(reg) {
+      .then(function (reg) {
         console.log('[Companion]', 'Service worker registered!');
         window.saved_reg = reg;
         window.register_push_service = utils.register_push_service;
-        utils.register_push_service(reg).then(function(){
-          window.close();
-        });
-    })
+
+        var serviceWorker;
+        if (reg.installing) {
+          serviceWorker = reg.installing;
+          // console.log('Service worker installing');
+        } else if (reg.waiting) {
+          serviceWorker = reg.waiting;
+          // console.log('Service worker installed & waiting');
+        } else if (reg.active) {
+          serviceWorker = reg.active;
+          // console.log('Service worker active');
+        }
+
+        if (serviceWorker) {
+          console.log("sw current state", serviceWorker.state);
+          if (serviceWorker.state == "activated") {
+            utils.register_push_service(reg).then(function () {
+              window.close();
+            });
+          }
+          serviceWorker.addEventListener("statechange", function (e) {
+            console.log("sw statechange : ", e.target.state);
+            if (e.target.state == "activated") {
+              utils.register_push_service(reg).then(function () {
+                window.close();
+              });
+            }
+          });
+        }
+      });
   }
 }
 
