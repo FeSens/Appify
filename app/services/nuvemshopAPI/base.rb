f5c15c5c18334171bcdd72a45e2c3efc0dc21f73
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-03-09 12:22:24 -0300

    API now working man

--- app/services/nuvemshopAPI/base.rb
+++ app/services/nuvemshopAPI/base.rb
@@ -3,15 +3,27 @@ module NuvemshopAPI
     class InvalidSessionError < StandardError; end
 
     self.include_format_in_path = false
-    self.headers['Authentication'] = 'bearer fa57087f9031185562d0ded379d7e93f98b20ebe'
     self.headers['User-Agent'] = 'Vorta (felipesensbonetto@gmail.com)'
     self.headers['Content-Type'] = 'application/json'
 
     class << self
+      threadsafe_attribute(:_api_version)
+      def headers
+        if _headers_defined?
+          _headers
+        elsif superclass != Object && superclass.headers
+          superclass.headers
+        else
+          _headers ||= {}
+        end
+      end
+
       def activate_session(session)
         raise InvalidSessionError, "Session cannot be nil" if session.nil?
         self.site = session.site
-        self.headers.merge!('Authentication' => "bearer #{session.token}")
+        self.headers['Authentication'] = "bearer #{session.token}"
+        print(self.headers)
+
       end
 
       def clear_session
