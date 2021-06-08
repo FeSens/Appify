6dbcddd6609c4e9f0d5dff8c53e7994b4be29876
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-06-08 14:51:39 -0300

    init optin counter controller

--- app/controllers/analytics/opt_ins_controller.rb
+++ app/controllers/analytics/opt_ins_controller.rb
@@ -1,14 +1,21 @@
 module Analytics
   class OptInsController < AnalyticsController
+    attr_accessor :opt_in_count
+
     def create
-      OptInCount.find_or_create_by(opt_in_params).increment
-      head :no_content
+      opt_in_count.increment(params[:action])
     end
 
     private
 
     def opt_in_params
-      params.permit(:service).merge(shop_id: shop.id, date: Time.now.beginning_of_hour)
+      params.permit(:service).merge(shop_id: shop.id, date: Date.today.at_beginning_of_month)
+    end
+
+    def opt_in_count
+      @opt_in_count ||= Rails.cache.fetch("Analytics/OptInsController/#{shop.id}/#{params[:service]}", expires_in: 60.seconds) do
+        OptInCount.find_or_create_by(opt_in_params)
+      end
     end
   end
 end
--- app/models/opt_in_count.rb
+++ app/models/opt_in_count.rb
@@ -1,7 +1,8 @@
 class OptInCount < ApplicationRecord
+  Include Countable
+
   belongs_to :shop
   enum service: { pwa: 0, push: 1 }
-  scope :last_hour, -> { where("created_at > ?", 1.hour.ago) }
 
   def increment
     OptInCount.increment_counter :count, id
