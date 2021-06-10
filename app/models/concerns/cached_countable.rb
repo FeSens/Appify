30206a78f542cc49d68155e1df9bcaee7ecf0d6f
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-06-09 23:34:18 -0300

    of this works.. this will fly

--- app/models/concerns/cached_countable.rb
+++ app/models/concerns/cached_countable.rb
@@ -7,14 +7,47 @@ module CachedCountable
     def self.cache_time(value)
       @@cache_time = value
     end
+
+    def self.increment_cached(attribute, id, by=1)
+      self.class.count_cached(:incrby, attribute, by, id)
+    end
+  
+    def self.decrement_cached(attribute, id, by=1)
+      self.class.count_cached(:decrby, attribute, by, id)
+    end
+
+    private
+
+    def self.count_cached(method, attribute, by, id)
+      redis_key = self.key(attribute, id)
+      _, _, queue = self.redis.multi do |multi|
+        multi.send(method, redis_key, by)
+        multi.sadd("CachedCountable", redis_key)
+        multi.set("CachedCountableQueued", 1, ex: @@cache_time + 2, nx: true)
+      end
+  
+      schedule if queue
+    end
+    
+    def self.redis
+      @@redis ||= Redis.current
+    end
+  
+    def self.key(attribute, id)
+      "#{name}/#{id}/#{attribute}"
+    end
+  
+    def self.schedule
+      CachedCountableJob.set(wait_until: @@cache_time.from_now).perform_later
+    end
   end
 
   def increment(attribute, by=1)
-    count_cached(:incrby, attribute, by)
+    self.class.count_cached(:incrby, attribute, by, id)
   end
 
   def decrement(attribute, by=1)
-    count_cached(:decrby, attribute, by)
+    self.class.count_cached(:decrby, attribute, by, id)
   end
 
   def increment!(attribute, by=1)
@@ -30,39 +63,10 @@ module CachedCountable
   end
 
   def is_cached?(attribute)
-    redis.get(key(attribute)).present?
+    self.class.redis.get(self.class.key(attribute, id)).present?
   end
 
   def get_current_value(attribute)
-    self[attribute] + redis.get(key(attribute)).to_i
-  end
-
-  private
-
-  def count_cached(method, attribute, by)
-    redis_key = key(attribute)
-    _, _, queue = redis.multi do |multi|
-      multi.send(method, redis_key, by)
-      multi.sadd("CachedCountable", redis_key)
-      multi.set("CachedCountableQueued", 1, ex: @@cache_time + 2, nx: true)
-    end
-
-    schedule if queue
-  end
-  
-  def redis
-    @redis ||= Redis.current
-  end
-
-  def class_name
-    self.class.name
-  end
-
-  def key(attribute=:count)
-    "#{class_name}/#{id}/#{attribute}"
-  end
-
-  def schedule
-    CachedCountableJob.set(wait_until: @@cache_time.from_now).perform_later
+    self[attribute] + self.class.redis.get(self.class.key(attribute, id)).to_i
   end
 end
--- app/services/analytics/campaigns/incrementor.rb
+++ app/services/analytics/campaigns/incrementor.rb
@@ -10,10 +10,14 @@ module Analytics
       end
 
       def call
-        Campaign.find(campaign_id).increment(column)
+        Campaign.increment_cached(column, campaign_id)
         return unless column == "impressions"
+        
+        push_interaction_id = Rails.cache.fetch("Analytics/Campaigns/Incrementor/#{shop_id}/#{Date.today.at_beginning_of_month}", expires_in: 60.seconds) do
+          PushInteraction.find_or_create_by(shop_id: shop_id, date: Date.today.at_beginning_of_month).id
+        end
 
-        PushInteraction.find_or_create_by(shop_id: shop_id, date: Date.today.at_beginning_of_month).increment(:count)
+        PushInteraction.increment_cached(:count, push_interaction_id)
       end
     end
   end
