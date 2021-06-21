d4833b2f093f5b85a34591eee30c2f7aaa570cfe
Felipe Bonetto <felipe.sens@aquila.ai>
2021-06-21 08:52:06 -0300

    remove clutter

--- app/controllers/demo/home_controller.rb
+++ app/controllers/demo/home_controller.rb
@@ -3,13 +3,5 @@ module Demo
     def index
       #Baixar App / Send Push
     end
-
-    def new
-      # Send Push, pop up promter if now alloed
-    end
-
-    def create
-      # Actually send push
-    end
   end
 end
