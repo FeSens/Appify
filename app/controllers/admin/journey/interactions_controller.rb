1e92d98b8ff4df6f931a0aa2c5cb0f63ac25ab5d
Felipe Bonetto <felipe.sens@aquila.ai>
2021-06-23 15:53:00 -0300

    remove pry

--- app/controllers/admin/journey/interactions_controller.rb
+++ app/controllers/admin/journey/interactions_controller.rb
@@ -2,7 +2,6 @@ module Admin
   module Journey
     class InteractionsController < AuthenticatedController
       def index
-        binding.pry
         @interactions = current_shop.journeys.find(params[:journey_id])
       end
     end
